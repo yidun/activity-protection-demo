8724f44a652adc7621591a78c927c90dbe4a2a41
hzliuqing <hzliuqing@>
2016-12-19 19:01:08 +0800

    新增demo

---
+++ activity-protection-check-demo/activity-protection-check-java-demo/src/main/java/com/netease/is/ac/ap/sdk/CheckResult.java
@@ -0,0 +1,16 @@
+package com.netease.is.ac.ap.sdk;
+
+public enum CheckResult {
+    /**
+     * 正常
+     */
+    NORMAL,
+    /**
+     * 嫌疑
+     */
+    SUSPECT,
+    /**
+     * 致命
+     */
+    FATAL
+}
