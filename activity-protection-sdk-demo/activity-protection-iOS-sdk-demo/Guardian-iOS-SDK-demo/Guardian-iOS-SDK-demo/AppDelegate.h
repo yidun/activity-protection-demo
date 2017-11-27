630f2974f1bd8fad47b72977ce4aa4e185d00b66
韩啸 <hanxiao@corp.netease.com>
2017-11-27 16:51:50 +0800

    提交V2.0 活动保护 iOS demo

---
+++ activity-protection-sdk-demo/activity-protection-iOS-sdk-demo/Guardian-iOS-SDK-demo/Guardian-iOS-SDK-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Guardian-iOS-SDK-demo
+//
+//  Created by NetEase on 2017/11/9.
+//  Copyright © 2017年 NetEase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ activity-protection-sdk-demo/activity-protection-iOS-sdk-demo/Guardian-iOS-SDK-demo/Guardian-iOS-SDK-demo/SyncViewController.h
@@ -0,0 +1,13 @@
+//
+//  SyncViewController.h
+//  Guardian-iOS-SDK-demo
+//
+//  Created by NetEase on 2017/11/9.
+//  Copyright © 2017年 NetEase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyncViewController : UIViewController
+
+@end
