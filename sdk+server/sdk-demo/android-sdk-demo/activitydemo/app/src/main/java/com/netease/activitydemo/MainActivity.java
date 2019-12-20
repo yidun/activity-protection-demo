517ec03ba8060e8b2cf9f965705c73729de308a8
wangzh7678 <170069212@qq.com>
2019-12-20 17:17:06 +0800

    Update MainActivity.java

--- sdk+server/sdk-demo/android-sdk-demo/activitydemo/app/src/main/java/com/netease/activitydemo/MainActivity.java
+++ sdk+server/sdk-demo/android-sdk-demo/activitydemo/app/src/main/java/com/netease/activitydemo/MainActivity.java
@@ -6,14 +6,20 @@ import android.view.View;
 import android.widget.Button;
 
 import com.netease.mobsec.rjsb.watchman;
-
+import com.netease.mobsec.rjsb.RequestCallback;
 
 public class MainActivity extends Activity {
     private Button mBtRise;
+    public statc String TAG= "TestLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        watchman.init(getApplicationContext(), "your productNumber");
+        watchman.init(mContext, "your productNumber",new RequestCallback(){
+            @Override
+            public void onResult(int code, String msg) {
+                Log.e(TAG,"init,code = " + code + " msg = " + msg);
+            }
+        });
         setContentView(R.layout.activity_main);
 
         mBtRise = (Button)findViewById(R.id.bt_rise);
