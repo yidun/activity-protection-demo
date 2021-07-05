ef714e85edfc53ac7f6921c9ead86961d7f7fb07
dun2021 <86950529+dun2021@users.noreply.github.com>
2021-07-05 15:44:04 +0800

    更新安卓demo

--- sdk+server/sdk-demo/android-sdk-demo/activitydemo/app/src/main/java/com/netease/activitydemo/MainActivity.java
+++ sdk+server/sdk-demo/android-sdk-demo/activitydemo/app/src/main/java/com/netease/activitydemo/MainActivity.java
@@ -6,40 +6,39 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.netease.mobsec.InitCallback;
 import com.netease.mobsec.WatchMan;
 import com.netease.mobsec.WatchManConf;
-import com.netease.mobsec.InitCallback;
-import com.netease.mobsec.GetTokenCallback;
 
 import static java.lang.Boolean.TRUE;
 
 public class MainActivity extends Activity {
     private Button mBtRise;
     public static String TAG = "TestLog";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         WatchManConf myconf = new WatchManConf();
         myconf.setCollectApk(TRUE);
         myconf.setCollectSensor(TRUE);
-        WatchMan.init(getApplicationContext(), "ProductNumber", myconf, new InitCallback() {
+        WatchMan.init(getApplicationContext(), "Product Number", myconf, new InitCallback() {
             @Override
             public void onResult(int code, String msg) {
-                Log.e(TAG,"init OnResult , code = " + code + " msg = " + msg);
+                Log.e(TAG, "init OnResult , code = " + code + " msg = " + msg);
             }
         });
         setContentView(R.layout.activity_main);
 
-        mBtRise = (Button)findViewById(R.id.bt_rise);
+        mBtRise = (Button) findViewById(R.id.bt_rise);
         mBtRise.setOnClickListener(listener);
     }
 
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if (view == mBtRise){
-                new ActivityTask(MainActivity.this){
-                }.execute();
+            if (view == mBtRise) {
+                new ActivityTask(MainActivity.this, "Secret ID", "BusinessID", "Secret Key").execute();
             }
         }
     };
